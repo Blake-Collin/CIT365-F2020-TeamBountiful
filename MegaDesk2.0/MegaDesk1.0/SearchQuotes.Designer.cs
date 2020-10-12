987ffff215186612974c1b0e0ec3aa0bbb9331dd
lancecwilson <wil17098@byui.edu>
2020-10-12 10:46:12 -0400

    Lance Updated

--- MegaDesk2.0/MegaDesk1.0/SearchQuotes.Designer.cs
+++ MegaDesk2.0/MegaDesk1.0/SearchQuotes.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(199, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Surface Material";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(79, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To see all quotes with the same surface material, choose the appropriate material" +
+    " and click SEARCH";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(383, 233);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "SearchQuotes";
             this.Text = "Search Quotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SearchQuotes_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
