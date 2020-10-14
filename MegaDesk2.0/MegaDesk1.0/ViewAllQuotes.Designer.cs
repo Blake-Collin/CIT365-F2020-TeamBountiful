0798778a2176f81bb3011372ecf7115d807e56bf
rudieadr <rudieadr@gmail.com>
2020-10-13 23:58:19 -0500

    datagridview code

--- MegaDesk2.0/MegaDesk1.0/ViewAllQuotes.Designer.cs
+++ MegaDesk2.0/MegaDesk1.0/ViewAllQuotes.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(91, 69);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(331, 250);
+            this.dataGridView.TabIndex = 0;
+
             // 
             // ViewAllQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.dataGridView);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ViewAllQuotes";
             this.Text = "View All Quotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ViewAllQuotes_FormClosed);
+            this.Load += new System.EventHandler(this.ViewAllQuotes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
--- MegaDesk2.0/MegaDesk1.0/ViewAllQuotes.cs
+++ MegaDesk2.0/MegaDesk1.0/ViewAllQuotes.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MegaDesk1._0
 {
@@ -15,6 +17,20 @@ namespace MegaDesk1._0
         public ViewAllQuotes()
         {
             InitializeComponent();
+
+            //different option
+            using (StreamReader file = File.OpenText("quotes.txt"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject oj = (JObject)JToken.ReadFrom(reader);
+            }
+            var quotes = JsonConvert.DeserializeObject("quotes.txt");
+            DataGridView.DataSource = quotes;
+            //another option
+
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject("quotes.txt");
+            var list = new BindingList<DeskQuote>(DeskQuote.dataQuotes);
+            DataGridView.DataSource = list;
         }
 
         private void ViewAllQuotes_FormClosed(object sender, FormClosedEventArgs e)
@@ -22,6 +38,11 @@ namespace MegaDesk1._0
             MainMenu main = (MainMenu)Tag;
             main.Show();
             Hide();
+        }
+
+        private void ViewAllQuotes_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
