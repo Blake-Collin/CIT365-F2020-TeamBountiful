d3a1f3a4c530abbd4b3cbc8f18a1fb28d8f1ed71
Collin Blake <collinblake1@gmail.com>
2020-11-06 21:43:16 -0700

    Final Touches

--- MegaDeskWeb/Models/RushShipping.cs
+++ MegaDeskWeb/Models/RushShipping.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +11,8 @@ namespace MegaDeskWeb.Models
     public class RushShipping
     {
         public int ID { get; set; }
+
+        [Display(Name = "Shipping Method")]
         public string RushName { get; set; }
         public decimal RushPrice { get; set; }
     }
